bdb59f70b8a724df7819b696bca2922f67eaeb2e
alisabrigok <alisabrigok@gmail.com>
2018-12-11 02:11:10 +0300

    Convert class based components

--- src/components/templates/Header/index.js
+++ src/components/templates/Header/index.js
@@ -1,21 +1,15 @@
 // @flow
 
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './Header.module.scss';
 import StatusTabContainer from '../../molecules/StatusTabContainer';
 import FilterContainer from '../../organisms/FilterContainer';
 
-type Props = {};
-
-class Header extends Component<Props> {
-  render() {
-    return (
-      <header className={styles.header}>
-        <StatusTabContainer />
-        <FilterContainer />
-      </header>
-    );
-  }
-}
+const Header = () => (
+  <header className={styles.header}>
+    <StatusTabContainer />
+    <FilterContainer />
+  </header>
+);
 
 export default Header;
